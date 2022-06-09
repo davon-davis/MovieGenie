494225f54a39f33bd9dd83eae37deb32f8fa98e7
davon <davon.l.davis.mil@army.mil>
2022-06-09 15:47:21 -0500

    second commit

---
+++ src/main/java/davon/davis/moviegenie/MoviegenieApplication.java
@@ -0,0 +1,13 @@
+package davon.davis.moviegenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviegenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviegenieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davon/davis/moviegenie/MoviegenieApplicationTests.java
@@ -0,0 +1,13 @@
+package davon.davis.moviegenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviegenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
